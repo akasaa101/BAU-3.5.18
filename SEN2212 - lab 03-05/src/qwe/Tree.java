d18ae608c3339d5dcefe75a8b0057fc84a2e5f60
Dany Montana <dany.montana@protonmail.com>
2018-03-05 13:41:20 +0000

    guns

--- SEN2212 - lab 03-05/src/qwe/Tree.java
+++ SEN2212 - lab 03-05/src/qwe/Tree.java
@@ -62,7 +62,7 @@ public class Tree {
 			t.leftChild = remove (key,t.rightChild);
 		else 
 			if (t.leftChild != null && t.rightChild != null){
-					t.setKey(findMax(t.leftChild).getKey());
+			//		t.getKey(findMax(t.leftChild).getKey());
 					t.leftChild=remove(t.getKey(),t.leftChild);
 		}
 		
@@ -87,4 +87,54 @@ public class Tree {
 		return findMax(t.rightChild);
 		
 	}
+	
+	public Node findMin(){
+		Node t=root;
+		if(t != null)
+			while(t.leftChild != null)
+				t = t.leftChild;
+		return t;
+	}
+	
+
+	
+	public void preorder(Node t){
+		if (t != null){
+			System.out.println(t.getKey()+ " ");
+			preorder(t.leftChild);
+			preorder(t.rightChild); }
+		}
+		
+		public void inorder (Node t){
+			if (t != null){
+				
+				inorder(t.leftChild);
+				System.out.println(t.getKey()+ " ");
+				inorder(t.rightChild);}
+			}
+			
+			public void postorder (Node t){
+				if (t != null){
+					
+					postorder(t.leftChild);
+					postorder(t.rightChild);
+					System.out.println(t.getKey()+ " ");
+				}
+				
+	}
+			
+			
+			public static void main (String [] args){
+				Node a = new Node (3,null,null);
+				Node b = new Node (3,null,null);
+				Node c = new Node (3,null,null);
+				Node d = new Node (3,null,null);
+				Node e = new Node (3,null,null);
+				Node f = new Node (3,null,null);
+			}
+	//preorder - root left right 6 3 2 0 1 5 4 16 8 20 
+	
+	//inorder left toor right  0 1 2 3 5 4 6 8 16 20
+	
+	//postorder left right root 1 0 2 4 5 3 8 20 16 6  
 }
